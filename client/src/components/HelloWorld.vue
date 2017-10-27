3ed350b3b25c2f185a5f128980ad07a5b6eaf846
Indraprahastha <indraprahastha@rocketmail.com>
2017-10-27 11:31:42 +0700

    Navbar

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="hello">
+    <div class="container">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img class="gambar" src="https://static.simomot.com/wp-content/uploads/2014/08/serangan-israel-ke-gaza.jpg" alt="Lights">
+        <div class="caption">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/nature.jpg">
+        <img class="gambar" src="http://soloposfm.com/assets/dokumen//2016/02/350332_03163326022015_jalan_rusak.jpg" alt="Nature">
+        <div class="caption">
+          <p>Lorem mesum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/fjords.jpg">
+        <img class="gambar" src="http://image.boombastis.com/images/2016/03/Gedung-Putih.jpg" alt="Fjords">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img class="gambar" src="https://static.simomot.com/wp-content/uploads/2014/08/serangan-israel-ke-gaza.jpg" alt="Lights">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/nature.jpg">
+        <img class="gambar" src="http://soloposfm.com/assets/dokumen//2016/02/350332_03163326022015_jalan_rusak.jpg" alt="Nature">
+        <div class="caption">
+          <p>Lorem mesum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/fjords.jpg">
+        <img class="gambar" src="http://image.boombastis.com/images/2016/03/Gedung-Putih.jpg" alt="Fjords">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img class="gambar" src="https://static.simomot.com/wp-content/uploads/2014/08/serangan-israel-ke-gaza.jpg" alt="Lights">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/nature.jpg">
+        <img class="gambar" src="http://soloposfm.com/assets/dokumen//2016/02/350332_03163326022015_jalan_rusak.jpg" alt="Nature">
+        <div class="caption">
+          <p>Lorem mesum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/fjords.jpg">
+        <img class="gambar" src="http://image.boombastis.com/images/2016/03/Gedung-Putih.jpg" alt="Fjords">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.gambar {
+  width: 100%;
+  height: 200px;
+}
+.caption {
+  text-align: justify;
+  font-size: 12px;
+  height: 100px;
+  overflow-y: auto;
+  /*overflow-x: scroll;*/
+}
+</style>
